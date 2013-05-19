c5bf32a6487986623fbd1a72c9246eceaf3db5b0
Chris Schmitz <ccschmitz@gmail.com>
2013-05-19 15:57:31 -0500

    Update commen author and title

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(function() {
     // closeComments();
 
     var text = window.getSelection().toString(),
-        $comments = $('<div id="commentator"><div class="comments"><div class="comment"><p>I think this warrants further discussion!</p><span>Posted 5 hours ago by <a href="#">Zach Gohr</a></span></div><div class="comment"><p>Well I think you\'re an idiot...</p><span>Posted 4 hours ago by <a href="#">Zach Gohr</a></div></div><form class="comment-box"><textarea placeholder="Enter your comment..." rows="1"></textarea><button type="submit">Submit Comment</button></form></div>');
+        $comments = $('<div id="commentator"><div class="comments"><div class="comment"><p>I think this warrants further discussion!</p><span>Posted 5 hours ago by <a href="#">Chris Schmitz</a></span></div><div class="comment"><p>Well I think you\'re an idiot...</p><span>Posted 4 hours ago by <a href="#">Zach Gohr</a></div></div><form class="comment-box"><textarea placeholder="Enter your comment..." rows="1"></textarea><button type="submit">Submit Comment</button></form></div>');
 
     $comments.find('textarea').css('overflow', 'hidden').autogrow({
       animate: false
