9f97dd7a6be2c9a1cf0b1aca060d31275d6fa385
Zach Gohr <zachary.gohr@gmail.com>
2013-05-28 16:37:47 -0500

    Removed all dependencies aside from Rangy, a project that serializes text selections in a few different ways, most impressively is the newest method it uses via character count. Turns out this is a very tough problem that hasnt really been solved, but it looks like rangy does it most closely. Text selections deserialized on IE or created on IE (tested on IE9) are off by a few characters.

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-var selectable = require('selectable');
+require('./vendor/rangy-core');  // Just a shim, so we get a 'rangy' global object
+require('./vendor/rangy-cssclassapplier');
+require('./vendor/rangy-highlighter');
 
 module.exports = Commentator;
 
-function Commentator() {
+function Commentator(rootNodeId) {
   var template = require('./template');
-  var selection = selectable('#content'),
-      d = document,
+  var d = document,
       div = d.createElement('div');
 
   div.innerHTML = template;
   var templ = div.firstChild;
 
-  selection.on('change', function(e){
+  rangy.init();
+
+  var highlighter = rangy.createHighlighter();
+  var classApplier = rangy.createCssClassApplier('someClass', {ignoreWhiteSpace: false, normalize: true})
+  highlighter.addClassApplier(classApplier);
+  highlighter.deserialize('type:textContent|87$285$1$someClass$')
+
+  d.body.onmouseup = function(e) {
     var popover = d.getElementById('commentator');
     if (popover) {
       d.body.removeChild(templ);
-    };
-
-    var text = window.getSelection().toString();
-    if (text) {
-      div.innerHTML = template;
-      d.body.appendChild(templ);
-    };
+    }
+    highlighter.highlightSelection('someClass');
+    var text = rangy.getSelection().toString();
+    d.body.appendChild(templ);
     console.log(e);
-  });
+  };
 }
