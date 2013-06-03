67fc628393e525384960bf72855233d531a6baf6
Chris Schmitz <ccschmitz@gmail.com>
2013-06-03 08:35:36 -0500

    Setup Grunt to autocompile the project

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+var shell = require('shelljs');
+
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>',
+    // Task configuration.
+    watch: {
+      js: {
+        options: { livereload: true },
+        files: ['*.js'],
+        tasks: ['exec:build_project']
+      },
+      css: {
+        options: { livereload: true },
+        files: ['css/sass/*.scss'],
+        tasks: ['sass:dev']
+      }
+    },
+    sass: {
+      dev: {
+        files: {
+          'css/main.css': 'css/sass/main.scss'
+        }
+      }
+    },
+    exec: {
+      build_project: {
+        command: 'make'
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Default task.
+  grunt.registerTask('default', ['sass:dev', 'exec:build_project']);
+
+};
--- Readme.md
+++ Readme.md
@@ -1,15 +1,18 @@
-
 # commentator
 
-  
+
 
 ## Installation
 
     $ component install ccschmitz/commentator
 
+To automatically compile your project when a file updates:
+
+    $ grunt watch
+
 ## API
 
-   
+
 
 ## License
 
