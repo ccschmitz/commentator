eda5c549efc4972c36d3a62c240e10962c144d42
Zach Gohr <zachary.gohr@gmail.com>
2013-06-05 09:35:32 -0500

    Jasmine tests with no runner yet. To test, use "make test"

---
+++ test/utils.spec.js
@@ -0,0 +1,12 @@
+
+describe('do something', function() {
+  var utils;
+
+  beforeEach(function() {
+    utils = require('../lib/utils');
+  });
+
+  it('exists', function() {
+    expect(utils).toBeDefined();
+  });
+});
